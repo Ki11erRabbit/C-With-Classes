4f0f00034b73e93e78a97d3ec0c45357ee8bff2e
ki11errabbit <ki11errabbit@posteo.net>
2022-09-04 00:19:57 -0600

    scanner should work with multiple header files, need to find way to break up operators into their own tokens.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+
+#define headerFile pair<string,vector<string>>
 
 using namespace std;
-int main(int argc, char *argv[]) {
 
-    std::cout << argc << " " << argv[1] << std::endl;
 
-    ifstream in(argv[1]);
+vector<string> scanFile(ifstream *in) {
     string line;
     vector<std::string> accumulator;
-    while (getline(in,line)) {
+    while (getline(*in,line)) {
 
         if (line[0] == '#') {
             if (line[line.size() - 1] == '\\') {
                 accumulator.push_back(line);
                 while (line[line.size() - 1] == '\\') {
-                    getline(in,line);
+                    getline(*in,line);
                     accumulator.push_back(line);
                 }
-
             }
             else {
                 accumulator.push_back(line);
             }
         }
+        else {
+            string token;
+            stringstream ss(line);
+
+            while (getline(ss,token, ' ')) {
+                if (token == "")
+                    continue;
+                accumulator.push_back(token);
+            }
+        }
+    }
+    return accumulator;
+}
+
+
+int main(int argc, char *argv[]) {
+
+    std::cout << argc << " " << argv[1] << std::endl;
+
+    vector<headerFile> headers;
+
+    for (int i = 1; i < argc; i++) {
+        ifstream in(argv[1]);
+        headers.push_back({argv[i],scanFile(&in)});
     }
 
 
@@ -33,8 +57,12 @@ int main(int argc, char *argv[]) {
 
 
 
-    for (auto body : accumulator) {
-        std::cout << body << std::endl;
+
+    for (auto header : headers) {
+        std::cout << header.first << std::endl;
+        for (auto body : header.second) {
+            cout << "\t" << body << endl;
+        }
     }
 
 
